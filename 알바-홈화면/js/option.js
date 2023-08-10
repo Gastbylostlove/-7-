c03fa5527c1085943faddbf7df88a4c26d8ed1ca
KIM NA YOUNG <141492611+naayong@users.noreply.github.com>
2023-08-10 17:37:57 +0900

    Delete 알바-홈화면 directory

--- 알바-홈화면/js/option.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const selectBusiness = document.getElementById("business");
-
-    selectBusiness.addEventListener("change", function() {
-        const selectedBusiness = selectBusiness.value;
-        console.log("선택된 사업장:", selectedBusiness);
-        // 여기에 선택된 사업장에 따른 동작을 추가하세요.
-    });
-});
