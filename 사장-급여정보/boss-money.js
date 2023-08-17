a27bc1b46e18a935ca26bec250b874f995a2b132
HYEBINess <jrfls@naver.com>
2023-08-17 21:47:22 +0900

    link_all_page

---
+++ 사장-급여정보/boss-money.js
@@ -0,0 +1,9 @@
+function redirectToPage() {
+    location.href =
+      "http://127.0.0.1:5500/team_soho/%EC%82%AC%EC%9E%A5%EB%8B%98_%EA%B7%BC%EB%AC%B4%EC%A0%95%EB%B3%B4/boss_calendar.html";
+  }
+
+  function redirectToPage1() {
+    location.href =
+    "http://127.0.0.1:5500/team_soho/%EC%82%AC%EC%9E%A5-%ED%99%88%ED%99%94%EB%A9%B4/boss-home.html";
+  }
---
+++ 알바-홈화면/part-timer-home.js
@@ -0,0 +1,19 @@
+// 근무관리 이동 : part_calendar.html
+function part_calendar() {
+    location.href =
+      "http://127.0.0.1:5500/team_soho/%EC%95%8C%EB%B0%94%EC%83%9D%EB%8B%AC%EB%A0%A5/part_calendar.html";
+  }
+
+
+//급여관리 이동  : pay.html
+  function pay() {
+    location.href =
+    "http://127.0.0.1:5500/team_soho/%EC%95%8C%EB%B0%94%EA%B8%89%EC%97%AC%EC%A0%95%EB%B3%B4/pay.html";
+  }
+
+
+  //선지급관리 이동
+  function salary_loan() {
+    location.href =
+    "http://127.0.0.1:5500/team_soho/%EC%95%8C%EB%B0%94-%EC%84%A0%EC%A7%80%EA%B8%89%EA%B4%80%EB%A6%AC/salary_loan.html";
+  }
