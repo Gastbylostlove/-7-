967acd6be4ab99b4b96c76ebbc9012c5205f1461
HYEBINess <jrfls@naver.com>
2023-08-18 16:25:56 +0900

    modified_link

---
+++ 사장-홈/boss-home.js
@@ -0,0 +1,23 @@
+//근무정보로 이동
+
+function redirectToPage() {
+  // currentPath = window.location.href;
+
+    // location.href =
+    href =
+      // "http://127.0.0.1:5500/%EC%82%AC%EC%9E%A5%EB%8B%98_%EA%B7%BC%EB%AC%B4%EC%A0%95%EB%B3%B4/boss_calendar.html";
+      "file://./사장-홈화면/boss-home.html";
+
+      
+      // 현재 페이지의 URL 가져오기
+      var currentHost = window.location.host;
+      console.log("Current Host:", currentHost);
+    
+      
+      
+  }
+
+  function redirectToPage1() {
+    location.href =
+    "http://127.0.0.1:5500/team_soho/%EC%82%AC%EC%9E%A5-%EA%B8%89%EC%97%AC%EC%A0%95%EB%B3%B4/boss-money.html";
+  }
--- 사장-홈화면/boss-home.js
+++
@@ -1,9 +0,0 @@
-function redirectToPage() {
-    location.href =
-      "http://127.0.0.1:5500/team_soho/%EC%82%AC%EC%9E%A5%EB%8B%98_%EA%B7%BC%EB%AC%B4%EC%A0%95%EB%B3%B4/boss_calendar.html";
-  }
-
-  function redirectToPage1() {
-    location.href =
-    "http://127.0.0.1:5500/team_soho/%EC%82%AC%EC%9E%A5-%EA%B8%89%EC%97%AC%EC%A0%95%EB%B3%B4/boss-money.html";
-  }
