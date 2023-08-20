871b780211deaae02bbead63ba61a5de7da33fb0
yoonji66 <91847313+yoonji66@users.noreply.github.com>
2023-08-20 23:51:50 +0900

    Add files via upload

---
+++ connection_.php
@@ -0,0 +1,16 @@
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$database = $_ENV['DB_DATABASE'];
+
+$con = mysqli_connect($host, $username, $password, $database);
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
